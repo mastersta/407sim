636ef1231de9e91c0cb2d522e90daa51eb9543c1
mastersta <mastersta90@gmail.com>
2021-01-30 20:59:22 -0500

    actually remove old file; fully flesh out test mode

--- src/407sim.ino
+++ src/407sim.ino
@@ -61,10 +61,10 @@ void setup() {
 
   //ledd setup
   tlcmanager.init();
-  tlcmanager.broadcast().set_milliams(20, 1000);
+  tlcmanager.broadcast().set_milliamps(20, 1000);
   
   //debugging
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
 }
 
@@ -116,18 +116,82 @@ void test_mode() {
   overhead_inputs_a = ~ioex_overhead1.readGPIOAB();
   overhead_inputs_b = ~ioex_overhead2.readGPIOAB();
 
+  
+  //get highest digital input pin number
+  static unsigned long all_digital_inputs_a = 0; //read LS 32 inputs
+  all_digital_inputs_a = (
+    (overhead_inputs_a<<24) +
+    (overhead_inputs_b<<16) +
+    (collective_inputs<<8) +
+    (cyclic_inputs)
+  );
+
+  static unsigned int all_digital_inputs_b = 0; //read remaining 16 inputs
+  all_digital_inputs_b = (
+    (panel_inputs_b) +
+    (panel_inputs_a) 
+  );
+  
+  unsigned int highest_input = 0; //run through MS inputs, bit by bit, to find first high input
+
+  for (i = 15; i >= 0; i--) {
+    if bitread(all_digital_inputs_b, 15) { //if current bit is high
+      highest_input = i + 32; //set as highest input, accounting for the LS set of inputs
+      break; //and break out
+    }
+    all_digital_inputs_b << i; //else shift it and loop around to look at the next bit
+  }
+
+  if highest_input = 0 { //only do if we had no high inputs from before
+    for (i = 32; i >= 0; i--) { //same thing as previous but with larger datatype
+      if bitread(all_digital_inputs_a, 31) {
+        highest_input = i;
+        break;
+      }
+      all_digital_inputs_a << i;
+    }
+  }
+
   //output data to annunciator
+  //set references to the drivers
+  static TLC59116 &ledd_panel_1 = tlcmanager[3];
+  static TLC59116 &ledd_panel_2 = tlcmanager[4];
+  static TLC59116 &ledd_panel_3 = tlcmanager[5];
+  
+  //blink the RPM low light to indicate that we're in test mode
+  ledd_panel_3.group_blink(3,1,128)
 
-
-
-
-
+  //set first 8 lights to represent highest input in binary
+  ledd_panel_1.on_pattern(highest_input<<8).off_pattern(~highest_input<<8);
+  
+  //set second 8 lights to reflect pwm values of analog axes
+  int pwm_values_a[] = {
+    cyclic_pitch,
+    (-1 * cyclic_pitch),
+    cyclic_roll,
+    (-1 * cyclic_roll),
+    collective,
+    (-1 * collective),
+    throttle,
+    (-1 * throttle),
+  }
+  //set following 6 lights for remaining axes
+  int pwm_values_b[] = {
+    antitorque,
+    (-1 * antitorque),
+    instrument_dimmer,
+    (-1 * instrument_dimmer),
+    rotor_brake,
+    (-1 * rotor_brake)
+  }
+  ledd_panel_1.set_outputs(8,14,pwm_values_a);
+  ledd_panel_1.set_outputs(0,7,pwn_values_b);
 }
 
 void safe_mode() {
   //plan is to remove all extraneous functions except for the analog inputs, just in case something
   //goes wrong during a flight. This will allow the operator to set the helicopter down under
-  //almost any circumstances and perform any other needed functions from inside the sim.
+  //almost any circumstances and then perform any other needed functions from inside the sim.
   //To indicate that we are in safe mode, the FADEC fail light will blink and all other annunciations
   //will be extinguished.
 }
