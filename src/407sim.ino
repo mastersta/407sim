80c72a49369c3232c79e6cfe8647b992655bdb82
mastersta <mastersta90@gmail.com>
2021-02-04 22:05:33 -0500

    rework basically the entire thing to use structs

--- src/407sim.ino
+++ src/407sim.ino
@@ -32,10 +32,20 @@
 #define addr_ledd_panel2 0x64
 #define addr_ledd_panel3 0x65
 
-Adafruit_ADS1015 anex_cyclic(addr_anex_cyclic);
-Adafruit_ADS1015 anex_collective(addr_anex_collective);
-Adafruit_ADS1015 anex_panel(addr_anex_panel);
-Adafruit_ADS1015 anex_overhead(addr_anex_overhead);
+
+
+
+struct anex {
+  Adafruit_ADS1015 cyclic(addr_anex_cyclic);
+  Adafruit_ADS1015 collective(addr_anex_collective);
+  Adafruit_ADS1015 panel(addr_anex_panel);
+  Adafruit_ADS1015 overhead(addr_anex_overhead);
+};
+
+struct anex anexmanager;
+
+
+
 
 struct ioex {
   Adafruit_MCP23017 cyclic;
@@ -46,10 +56,23 @@ struct ioex {
   Adafruit_MCP23017 overhead1;
   Adafruit_MCP23017 overhead2;
   Adafruit_MCP23017 overhead3;
-}
+};
+
 struct ioex ioexmanager;
 
+
+
+
 TLC59116Manager tlcmanager(Wire, i2c_speed);
+
+struct ledd {
+  TLC59116 &panel1 = tlcmanager[3];
+  TLC59116 &panel2 = tlcmanager[4];
+  TLC59116 &panel3 = tlcmanager[5];
+};
+
+struct ledd leddmanager;
+
 
 //init joystick
 Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
@@ -59,6 +82,25 @@ Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
   true,true,          //rudder(a/t), throttle(throttle)
   true,true,false  //accelerator(collective), brake(rotor brake), steering
   );
+
+
+
+struct struct_anex_values {  //stores analog input values
+  int cyclic[4];
+  int collective[4];
+  int panel[4];
+  int overhead[4];
+};
+
+struct anex_input_values struct_anex_values { //init to zero
+  {0,0,0,0},
+  {0,0,0,0},
+  {0,0,0,0},
+  {0,0,0,0}
+};
+
+
+
 
 //type of input for every pin on each ioex
 //0 for unused/NYI, 1 for momentary, 2 for toggle, 3 for encoder phase A, 4 for encoder phase B.
@@ -72,7 +114,7 @@ struct struct_ioex_type { //struct to ease passing through functions
   byte overhead1[16];
   byte overhead2[16];
   byte overhead3[16];
-}
+};
 
 const struct ioex_input_types struct_ioex_type {
   {1,1,1,1,1,0,0,0,0,1,0,0,0,0,0,0}, //cyclic
@@ -83,7 +125,7 @@ const struct ioex_input_types struct_ioex_type {
   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}, //overhead1
   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}, //overhead2
   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}  //overhead3
-}
+};
 
 //TODO: see which is required; above should work (and I would prefer it due to const) but
 //leaving this here just in case
@@ -97,12 +139,31 @@ const struct ioex_input_types struct_ioex_type {
     //ioex_input_types.overhead2 =   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
     //ioex_input_types.overhead3 =   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
 
+
+
+
+struct struct_ioex_values { //struct to ease passing through functions, used to store read values
+  unsigned int cyclic;
+  unsigned int collective;
+  unsigned int panel1;
+  unsigned int panel2;
+  unsigned int panel3;
+  unsigned int overhead1;
+  unsigned int overhead2;
+  unsigned int overhead3;
+};
+
+struct ioex_input_values struct_ioex_values {0,0,0,0,0,0,0,0}; //init at 0
+
+
+
 void setup() {
+
   //anex setup
-  anex_cyclic.begin();
-  anex_collective.begin();
-  anex_panel.begin();
-  anex_overhead.begin();
+  anexmanager.cyclic.begin();
+  anexmanager.collective.begin();
+  anexmanager.panel.begin();
+  anexmanager.overhead.begin();
 
   //ioex setup
   ioexmanager.cyclic.begin(addr_ioex_cyclic - addr_ioex_offset);
@@ -116,14 +177,15 @@ void setup() {
   tlcmanager.init();
   tlcmanager.broadcast().set_milliamps(20, 1000);
   
-  //debugging
-  //Serial.begin(9600);
-
 }
 
+
+
+
+int hat_direction(int input_array) {
 //takes in an array of the four hat switch states (up, right, down, left) already inverted so 1 = engaged
 //outputs the degrees that the joystick library expects for the hat position
-int hat_direction(int input_array) {
+
   int output_array[4] = {
     input_array[0],
     input_array[1] * 2,
@@ -169,6 +231,40 @@ int hat_direction(int input_array) {
   }
 }
 
+
+
+
+bool read_all_analogs() {  //sets global anex_input_values struct
+  anex_input_values.cyclic[0] =       anexmanager.cyclic.readADC_SingleEnded(0);     //pitch
+  anex_input_values.cyclic[1] =       anexmanager.cyclic.readADC_SingleEnded(1);     //roll
+  anex_input_values.collective[0] =   anexmanager.collective.readADC_SingleEnded(0); //collective
+  anex_input_values.collective[1] =   anexmanager.collective.readADC_SingleEnded(1); //throttle
+  anex_input_values.panel[0] =        anexmanager.panel.readADC_SingleEnded(0);      //antitorque
+  anex_input_values.panel[1] =        anexmanager.panel.readADC_SingleEnded(1);      //gtn1 vol
+  anex_input_values.panel[2] =        anexmanager.panel.readADC_SingleEnded(2);      //gtn2 vol
+  anex_input_values.overhead[0] =     anexmanager.overhead.readADC_SingleEnded(0);   //instrument dimmer
+  anex_input_values.overhead[1] =     anexmanager.overhead.readADC_SingleEnded(1);   //rotor brake
+  return true;  //if something goes wrong, might return false and we can use that to go to safe mode
+};
+
+
+
+
+bool read_all_digitals() {  //invert due to pullups
+  ioex_input_values.cyclic =     ~ioexmanager.cyclic.readGPIOAB();
+  ioex_input_values.collective = ~ioexmanager.collective.readGPIOAB();
+  ioex_input_values.panel1 =     ~ioexmanager.panel1.readGPIOAB();
+  ioex_input_values.panel2 =     ~ioexmanager.panel2.readGPIOAB();
+  ioex_input_values.panel3 =     ~ioexmanager.panel3.readGPIOAB();
+  ioex_input_values.overhead1 =  ~ioexmanager.overhead1_inputs.readGPIOAB();
+  ioex_input_values.overhead2 =  ~ioexmanager.overhead2_inputs.readGPIOAB();
+  ioex_input_values.overhead3 =  ~ioexmanager.overhead3_inputs.readGPIOAB();
+  return true;  //if something goes wrong, might return false and we can use that to go to safe mode
+};
+
+
+
+
 void test_mode() {
   //plan is to get highest digital input pin number (based on wire numbering diagram) and output that
   //pin number, in binary, to the annunciator; lights will be lit up in order to represent that pin
@@ -178,72 +274,32 @@ void test_mode() {
   //to a pc. A hardware switch, directly connected to a pin on the arduino, may be implemented to
   //engage this mode. 
 
-  //declare analog inputs
-  static int cyclic_pitch;
-  static int cyclic_roll;
-  static int collective;
-  static int throttle;
-  static int antitorque;
-  static int gtn1_vol;
-  static int gtn2_vol;
-  static int instrument_dimmer;
-  static int rotor_brake;
 
-  //declare digital inputs
-  static unsigned int cyclic_inputs;
-  static unsigned int collective_inputs;
-  static unsigned int panel_inputs_a;
-  static unsigned int panel_inputs_b;
-  static unsigned int overhead_inputs_a;
-  static unsigned int overhead_inputs_b;
+  if read_all_analogs() {}; //ignore return, we don't care if something goes wrong
+  if read_all_digitals() {};
 
-  //declare digital outputs
-  static unsigned int annunciator_output1;
-  static unsigned int annunciator_output2;
 
-  //read all analogs
-  cyclic_pitch =  anex_cyclic.readADC_SingleEnded(0);
-  cyclic_roll =  anex_cyclic.readADC_SingleEnded(1);
-  collective =  anex_collective.readADC_SingleEnded(0);
-  throttle =  anex_collective.readADC_SingleEnded(1);
-  antitorque =  anex_panel.readADC_SingleEnded(0);
-  gtn1_vol = anex_panel.readADC_SingleEnded(1);
-  gtn2_vol = anex_panel.readADC_SingleEnded(2);
-  instrument_dimmer = anex_overhead.readADC_SingleEnded(0);
-  rotor_brake =  anex_overhead.readADC_SingleEnded(1);
-
-  //TODO: redo this section to use struct
-
-  //read all digitals
-  cyclic_inputs = ~ioexmanager.cyclic.readGPIOAB();
-  collective_inputs = ~ioexmanager.collective.readGPIOAB();
-  panel1_inputs = ~ioexmanager.panel1.readGPIOAB();
-  panel2_inputs = ~ioexmanager.panel2.readGPIOAB();
-  panel3_inputs = ~ioexmanager.panel3.readGPIOAB();
-  overhead1_inputs = ~ioexmanager.overhead1_inputs.readGPIOAB();
-  overhead2_inputs = ~ioexmanager.overhead2_inputs.readGPIOAB();
-  overhead3_inputs = ~ioexmanager.overhead3_inputs.readGPIOAB();
-
-  
   //get highest digital input pin number
   static unsigned long all_digital_inputs_a = 0; //read LS 32 inputs
   all_digital_inputs_a = (
-    (overhead_inputs_a<<24) +
-    (overhead_inputs_b<<16) +
-    (collective_inputs<<8) +
-    (cyclic_inputs)
+    (ioex_input_values.panel2<<24) +
+    (ioex_input_values.panel1<<16) +
+    (ioex_input_values.collective<<8) +
+    (ioex_input_values.cyclic)
   );
 
-  static unsigned int all_digital_inputs_b = 0; //read remaining 16 inputs
+  static unsigned long all_digital_inputs_b = 0; //read MS 32 inputs
   all_digital_inputs_b = (
-    (panel_inputs_b) +
-    (panel_inputs_a) 
+    (ioex_input_values.overhead3<<24)
+    (ioex_input_values.overhead2<<16) +
+    (ioex_input_values.overhead1<<8) +
+    (ioex_input_values.panel3) +
   );
   
   unsigned int highest_input = 0; //run through MS inputs, bit by bit, to find first high input
 
-  for (i = 15; i >= 0; i--) {
-    if bitread(all_digital_inputs_b, 15) { //if current bit is high
+  for (i = 32; i >= 0; i--) {
+    if bitread(all_digital_inputs_b, 31) { //if current bit is high
       highest_input = i + 32; //set as highest input, accounting for the LS set of inputs
       break; //and break out
     }
@@ -251,7 +307,7 @@ void test_mode() {
   }
 
   if highest_input = 0 { //only do if we had no high inputs from before
-    for (i = 32; i >= 0; i--) { //same thing as previous but with larger datatype
+    for (i = 32; i >= 0; i--) { //same thing as previous
       if bitread(all_digital_inputs_a, 31) {
         highest_input = i;
         break;
@@ -260,37 +316,46 @@ void test_mode() {
     }
   }
 
+
+
+
   //output data to annunciator
-  //set references to the drivers
-  static TLC59116 &ledd_panel_1 = tlcmanager[3];
-  static TLC59116 &ledd_panel_2 = tlcmanager[4];
-  static TLC59116 &ledd_panel_3 = tlcmanager[5];
+
   
   //blink the RPM low light to indicate that we're in test mode
-  ledd_panel_3.group_blink(3,1,128);
+  leddmanager.panel3.group_blink(3,1,128);
 
   //set first 8 lights to represent highest input in binary
-  ledd_panel_1.on_pattern(highest_input<<8).off_pattern(~highest_input<<8);
+  leddmanager.panel1.on_pattern(highest_input<<8).off_pattern(~highest_input<<8);
   
   //set second 8 lights to reflect pwm values of analog axes
   int pwm_values_a[8] = {
-    map(cyclic_pitch,-2048,2048,0,256),
-    map(cyclic_roll,-2048,2048,0,256),
-    map(collective,-2048,2048,0,256),
-    map(throttle,-2048,2048,0,256),
-    map(antitorque,-2048,2048,0,256),
-    map(gtn1_vol,-2048,2048,0,256),
-    map(gtn2_vol,-2048,2048,0,256),
-    map(instrument_dimmer,-2048,2048,0,256)
+    map(anex_input_values.cyclic[0],      -2048,2048,0,256), //pitch
+    map(anex_input_values.cyclic[1],      -2048,2048,0,256), //roll
+    map(anex_input_values.collective[0],  -2048,2048,0,256), //collective
+    map(anex_input_values.collective[1],  -2048,2048,0,256), //throttle
+    map(anex_input_values.panel[0],       -2048,2048,0,256), //antitorque
+    map(anex_input_values.panel[1],       -2048,2048,0,256), //gtn1 vol
+    map(anex_input_values.panel[2],       -2048,2048,0,256), //gtn2 vol
+    map(anex_input_values.overhead[0]     -2048,2048,0,256), //instrument dimmer
   };
+
   //set following 1 light for remaining axis
   int pwm_values_b[1] = {
-    map(rotor_brake,-2048,2048,0,256)
+    map(anex_input_values.overhead[1],    -2048,2048,0,256)  //rotor brake
   };
-  ledd_panel_1.set_outputs(8,15,pwm_values_a);
-  ledd_panel_1.set_outputs(0,0,pwn_values_b);
+
+  //set following lights for analog inputs
+  leddmanager.panel1.set_outputs(8,15,pwm_values_a);
+  leddmanager.panel1.set_outputs(0,0,pwn_values_b);
+
+
 }
 
+
+
+
+bool go_to_safe_mode = false;
 void safe_mode() {
   //plan is to remove all extraneous functions except for the analog inputs, just in case something
   //goes wrong during a flight. This will allow the operator to set the helicopter down under
@@ -298,26 +363,20 @@ void safe_mode() {
   //To indicate that we are in safe mode, the FADEC fail light will blink and all other annunciations
   //will be extinguished.
 
-  //initialize
-  static int cyclic_pitch;
-  static int cyclic_roll;
-  static int collective;
-  static int throttle;
-  static int antitorque;
 
   //read
-  cyclic_pitch =  anex_cyclic.readADC_SingleEnded(0);
-  cyclic_roll =  anex_cyclic.readADC_SingleEnded(1);
-  collective =  anex_collective.readADC_SingleEnded(0);
-  throttle =  anex_collective.readADC_SingleEnded(1);
-  antitorque =  anex_panel.readADC_SingleEnded(0);
+  anex_input_values.cyclic[0] =       anexmanager.cyclic.readADC_SingleEnded(0);     //pitch
+  anex_input_values.cyclic[1] =       anexmanager.cyclic.readADC_SingleEnded(1);     //roll
+  anex_input_values.collective[0] =   anexmanager.collective.readADC_SingleEnded(0); //collective
+  anex_input_values.collective[1] =   anexmanager.collective.readADC_SingleEnded(1); //throttle
+  anex_input_values.panel[0] =        anexmanager.panel.readADC_SingleEnded(0);      //antitorque
   
   //output
-  joystick.setYAxis(cyclic_pitch);
-  joystick.setXAxis(cyclic_roll);
-  joystick.setRudder(antitorque);
-  joystick.setThrottle(throttle);
-  joystick.setAccelerator(collective);
+  joystick.setYAxis                   (anex_input_values.cyclic[0]);
+  joystick.setXAxis                   (anex_input_values.cyclic[1]);
+  joystick.setRudder                  (anex_input_values.collective[0]);
+  joystick.setThrottle                (anex_input_values.collective[1]);
+  joystick.setAccelerator             (anex_input_values.panel[0]);
 
   //blink fadec
   static TLC59116 &ledd_panel_2 = tlcmanager[4];
@@ -327,102 +386,70 @@ void safe_mode() {
 
 }
 
+
+
+
 void loop() {
   //standard operations consisting of reading all analog and digital inputs, and sending them to the
   //sim as a joystick; followed by reading the air manager message port and using that data to
   //properly light up the annunciator
 
-  //analog inputs - output values directly to sim
-  //read
-  cyclic_pitch =  anex_cyclic.readADC_SingleEnded(0);
-  cyclic_roll =  anex_cyclic.readADC_SingleEnded(1);
-  collective =  anex_collective.readADC_SingleEnded(0);
-  throttle =  anex_collective.readADC_SingleEnded(1);
-  antitorque =  anex_panel.readADC_SingleEnded(0);
-  
-  //output
-  joystick.setYAxis(cyclic_pitch);
-  joystick.setXAxis(cyclic_roll);
-  joystick.setRudder(antitorque);
-  joystick.setThrottle(throttle);
-  joystick.setAccelerator(collective);
+  if read_all_analogs() {
 
-  //read all digital inputs now
-  //declare digital inputs
-  static unsigned int cyclic_inputs;
-  static unsigned int collective_inputs;
-  static unsigned int panel_inputs_a;
-  static unsigned int panel_inputs_b;
-  static unsigned int overhead_inputs_a;
-  static unsigned int overhead_inputs_b;
+    joystick.setYAxis                   (anex_input_values.cyclic[0]);
+    joystick.setXAxis                   (anex_input_values.cyclic[1]);
+    joystick.setRudder                  (anex_input_values.collective[0]);
+    joystick.setThrottle                (anex_input_values.collective[1]);
+    joystick.setAccelerator             (anex_input_values.panel[0]);
 
-  //read all digitals
-  cyclic_inputs = ~ioex_cyclic.readGPIOAB();
-  collective_inputs = ~ioex_collective.readGPIOAB();
-  panel_inputs1 = ~ioex_panel1.readGPIOAB();
-  panel_inputs2 = ~ioex_panel2.readGPIOAB();
-  panel_inputs3 = ~ioex_panel3.readGPIOAB();
-  overhead_inputs1 = ~ioex_overhead1.readGPIOAB();
-  overhead_inputs2 = ~ioex_overhead2.readGPIOAB();
-  overhead_inputs3 = ~ioex_overhead3.readGPIOAB();
-
-  //place into array for easy access
-  unsigned int inputs_array[8] {
-    cyclic_inputs,
-    collective_input,
-    panel_inputs1,
-    panel_inputs2,
-    panel_inputs3,
-    overhead_inputs1,
-    overhead_inputs2,
-    overhead_inputs3
-  }
-
-  //hat switches - see hat switch function
-  static int cyclic_hat_array[4] {
-    bitRead(cyclic_inputs, 5), //up
-    bitRead(cyclic_inputs, 6), //right
-    bitRead(cyclic_inputs, 8), //down
-    bitRead(cyclic_inputs, 7), //left
-  }
-  joystick.setHatSwitch(0, hat_direction(cyclic_hat_array));
-
-  static int collective_hat_array[4] {
-    bitRead(collective_inputs, 5), //up
-    bitRead(collective_inputs, 6), //right
-    bitRead(collective_inputs, 8), //down
-    bitRead(collective_inputs, 7), //left
-  }
-  joystick.setHatSwitch(1, hat_direction(collective_hat_array));
+  } else {go_to_safe_mode = true;};
 
 
-  //loops through all the inputs, checks the type of input, and applies the correct action
 
-  //loop through each ioex first
-  for (int i = 0; i < 6; i++) {
+  if read_all_digitals() {
 
-    //and then through each input in the ioex
-    for (int j = 0; j < 16; j++) {
 
-      //check type
-      if button_types[i][j] = 1 { //momentary
-        //set button (number) based on what got read earlier
-        Joystick.setButton((i*j), bitRead(inputs_array[i], j);
-
-      } else if button_types[i][j] = 2 { //toggle
-      //TODO
-        //need to store release time for each toggle and press state
-        //check if current time is past the release time
-          //if it is past and the state is false and the switch is active, we're in a new press event, so:
-            //send a press, set a new release time, and set state true
-          //if it is and the state is true, we're ready to release, so:
-            //send release   (check state first to help with short circuiting)
-          //if not and state is true, we're still waiting for release time so:
-            //do nothing
-            
-      } else if button_types[i][j] = 3 { //encoder
-        //TODO
-      };
+    //hat switches
+    static int cyclic_hat_array[4] {
+      bitRead(ioex_input_values.cyclic, 5), //up
+      bitRead(ioex_input_values.cyclic, 6), //right
+      bitRead(ioex_input_values.cyclic, 8), //down
+      bitRead(ioex_input_values.cyclic, 7) //left
     }
-    //TODO: something about checking if we're running fast enough, and displaying a warning if not
+    joystick.setHatSwitch(0, hat_direction(cyclic_hat_array));
+
+
+    static int collective_hat_array[4] {
+      bitRead(ioex_input_values.collective, 5), //up
+      bitRead(ioex_input_values.collective, 6), //right
+      bitRead(ioex_input_values.collective, 8), //down
+      bitRead(ioex_input_values.collective, 7), //left
+    }
+    joystick.setHatSwitch(1, hat_direction(collective_hat_array));
+
+
+
+
+
+    //need to store release time for each toggle and press state
+    //check if current time is past the release time
+      //if it is past and the state is false and the switch is active, we're in a new press event, so:
+        //send a press, set a new release time, and set state true
+      //if it is and the state is true, we're ready to release, so:
+        //send release   (check state first to help with short circuiting)
+      //if not and state is true, we're still waiting for release time so:
+        //do nothing
+
+
+
+  } else {go_to_safe_mode = true;};
+
+
+
+
+  if go_to_safe_mode {
+    safe_mode();
   }
+
+
+}
