5e9a1e4434bf937e96c568adc79d45c026741132
Tyler Masters <mastersta90@gmail.com>
2021-02-19 16:06:30 -0500

    ledd attempt 1

--- ledd_test.ino
+++ ledd_test.ino
@@ -2,44 +2,17 @@
 #include <Wire.h>
 #include <TLC59116_Unmanaged.h>
 #include <TLC59116.h>
-#include <si_base.h>
-#include <si_network.h>
-#include <si_circular_data.h>
-#include <si_message_port.h>
-#include <sim_extern_shared.h>
-#include <sim_extern_client.h>
-#include <si_input_buffer.h>
-#include <si_message_port_driver.h>
-#include <si_output_buffer.h>
+#include <si_message_port.hpp>
 
 //misc defines
 #define i2c_speed 100000 //increase later after testing
 
 //ledd = TLC59017 led driver
-//#define addr_ledd_overhead1 0x60
-//#define addr_ledd_overhead2 0x61
-//#define addr_ledd_overhead3 0x62
 #define addr_ledd_panel1 0
 #define addr_ledd_panel2 1
 #define addr_ledd_panel3 2
 
 TLC59116Manager tlcmanager(Wire, i2c_speed);
-
-SiMessagePort* messagePort;
-
-static void new_message_callback(uint16_t message_id, struct SiMessagePortPayload* payload) {
-  //message recieved
-
-  uint16_t annunciator_data[3];
-
-  if (message_id < 3) { //0, 1, and 2 should be annunciator data
-    annunciator_data[message_id] = payload->data_int //cast from uint_32 to uint_16 should chop off MSBs
-    leddmanager.on_pattern(annunciator_data[message_id]); //actually put it on the annunciator
-    leddmanager.off_pattern(~annunciator_data[message_id];
-  };
-    
-  }
-}
 
 struct ledd {
   TLC59116 &panel1 = tlcmanager[addr_ledd_panel1];
@@ -48,6 +21,31 @@ struct ledd {
 };
 
 struct ledd leddmanager;
+
+SiMessagePort* messagePort;
+
+static void new_message_callback(uint16_t message_id, struct SiMessagePortPayload* payload) {
+  //message recieved
+
+  uint16_t annunciator_data[3];
+
+  if (message_id == 0) { //0, 1, and 2 should be annunciator data
+    annunciator_data[message_id] = payload->data_int; //cast from uint_32 to uint_16 should chop off MSBs
+    leddmanager.panel1.on_pattern(annunciator_data[message_id]); //actually put it on the annunciator
+    leddmanager.panel1.off_pattern(~annunciator_data[message_id]);
+    
+  } else if (message_id == 1) {
+    annunciator_data[message_id] = payload->data_int; //cast from uint_32 to uint_16 should chop off MSBs
+    leddmanager.panel2.on_pattern(annunciator_data[message_id]); //actually put it on the annunciator
+    leddmanager.panel2.off_pattern(~annunciator_data[message_id]);
+    
+  } else if (message_id == 2) {
+    
+    annunciator_data[message_id] = payload->data_int; //cast from uint_32 to uint_16 should chop off MSBs
+    leddmanager.panel3.on_pattern(annunciator_data[message_id]); //actually put it on the annunciator
+    leddmanager.panel3.off_pattern(~annunciator_data[message_id]);
+  };
+};
 
 
 void setup() {
@@ -73,4 +71,3 @@ void loop() {
 
   //TODO: AM Gauge creation
 }
-
