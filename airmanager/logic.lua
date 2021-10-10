c971c0a4f8e74a8425b39d9011db05cffa296e68
mastersta <mastersta90@gmail.com>
2021-10-10 19:45:53 -0400

    Unbork encoder fix

--- airmanager/logic.lua
+++ airmanager/logic.lua
@@ -99,19 +99,19 @@ function incoming_message_callback(id, payload)
 
   if id == 2 then
 
-    for index, value in ipairs(payload) do   
+    for index, value in ipairs(encoder_table) do
 
-      difference = encoder_table[index].previous - value
+      difference = encoder_table[index].previous - payload[index]
       new_value = encoder_table[index].simvalue + (difference * encoder_table[index].increment)
 
-      if difference != 0 then
+      if difference ~= 0 then
         xpl_dataref_write(
           encoder_table[index].dataref,
           encoder_table[index].type,
           new_value
         )
 
-        encoder_table[index].previous = value
+        encoder_table[index].previous = payload[index]
       end
     end
   end
@@ -133,7 +133,7 @@ end --function end
 
 
 
-encoder_table{
+encoder_table = {
   [1] = {
     ["simvalue"] = 29.92,
     ["dataref"] = "sim/cockpit2/gauges/actuators/barometer_setting_in_hg_pilot",
@@ -163,11 +163,13 @@ function encoder_update(dataref1, dataref2, dataref3)
   encoder_table[3].simvalue = dataref3
 end
 
+hw_id = hw_message_port_add("ARDUINO_LEONARDO_A", incoming_message_callback)
+
 xpl_dataref_subscribe(
-  encoder_table[1].dataref, encoder_table[1].value,
-  encoder_table[2].dataref, encoder_table[2].value,
-  encoder_table[3].dataref, encoder_table[3].value,
-  encoder_update_callback
+  encoder_table[1].dataref, encoder_table[1].type,
+  encoder_table[2].dataref, encoder_table[2].type,
+  encoder_table[3].dataref, encoder_table[3].type,
+  encoder_update
 )
 
 
