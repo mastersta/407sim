f0b7bb45201b4869caca033e87b349e351099de5
Tyler Masters <mastersta90@gmail.com>
2021-10-16 18:04:54 -0400

    inital pass on circuit breaker implementation

--- airmanager/lib/switch_functions.lua
+++ airmanager/lib/switch_functions.lua
@@ -243,6 +243,234 @@ function switch_2h8unused(state)
 end
 
 
+function cb_fuelvalve(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/FUEL_VALVE",
+    state, 0
+  )
+end
+
+function cb_fuelqty(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/FUEL_QTY",
+    state, 0
+  )
+end
+
+function cb_fuelpressure(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/FUEL_PRESS",
+    state, 0
+  )
+end
+
+function cb_genreset(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/GEN_RESET",
+    state, 0
+  )
+end
+
+function cb_genfield(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/GEN_FIELD",
+    state, 0
+  )
+end
+
+function cb_xmsntemp(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/XMSN_TEMP",
+    state, 0
+  )
+end
+
+function cb_xmsnpress(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/XMSN_PRESS",
+    state, 0
+  )
+end
+
+function cb_engmgt(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_MGT",
+    state, 0
+  )
+end
+
+function cb_engtrq(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_TRQ",
+    state, 0
+  )
+end
+
+function cb_engng(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_NG",
+    state, 0
+  )
+end
+
+function cb_engnp(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_NP",
+    state, 0
+  )
+end
+
+function cb_engnr(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_NR",
+    state, 0
+  )
+end
+
+function cb_engoiltemp(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_TEMP",
+    state, 0
+  )
+end
+
+function cb_engoilpress(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ENG_PRESS",
+    state, 0
+  )
+end
+
+function cb_engantiice(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/ANTI_ICE",
+    state, 0
+  )
+end
+
+function cb_starter(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/START",
+    state, 0
+  )
+end
+
+function cb_igniter(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/IGNITER",
+    state, 0
+  )
+end
+
+function cb_fadec(state)
+  output = (1 - state) * 6
+  xpl_dataref_write(
+    "sim/operation/failures/rel_fadec_0"
+    output, 0
+  )
+end
+
+function cb_hydsys(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/HYD_SYS",
+    state, 0
+  )
+end
+
+function cb_pedalstop(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/PEDAL_STOP",
+    state, 0
+  )
+end
+
+function cb_ldglights(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/LIGHT_PWR",
+    state, 0
+  )
+end
+
+function cb_ldglightscont(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/LIGHT_CONT",
+    state, 0
+  )
+end
+
+function cb_cautionlights(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/LIGHT_CAUT",
+    state, 0
+  )
+end
+
+function cb_oatv(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/OAT_V",
+    state, 0
+  )
+end
+
+function cb_amps(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/AMPS",
+    state, 0
+  )
+end
+
+function cb_navcom1(state)
+  output = (1 - state) * 6
+  xpl_dataref_write(
+    "sim/operation/failures/rel_navcom1",
+    output, 0
+  )
+end
+
+function cb_com2(state)
+  output = (1 - state) * 6
+  xpl_dataref_write(
+    "sim/operation/failures/rel_navcom2",
+    output, 0
+  )
+end
+
+function cb_xpndr(state)
+  output = (1 - state) * 6
+  xpl_dataref_write(
+    "sim/operation/failures/rel_xpndr",
+    output, 0
+  )
+end
+
+function cb_gps1(state)
+  output = (1 - state) * 6
+  xpl_dataref_write(
+    "sim/operation/failures/rel_gps",
+    output, 0
+  )
+end
+
+function cb_gps2(state)
+  output = (1 - state) * 6
+  xpl_dataref_write(
+    "sim/operation/failures/rel_gps2",
+    output, 0
+  )
+end
+
+function cb_radalt(state)
+  xpl_dataref_write(
+    "B407/CircuitBreaker/RADAR_ALT",
+    state, 0
+  )
+end
+
+function cb_4h8unused(state)
+end
+
+
+
 switch_table = {
   [1] = {
     switch_fuelvalve,
@@ -283,5 +511,45 @@ switch_table = {
     switch_instrumentatt,
     switch_instrumentturn,
     switch_2h8unused
+  },
+  [5] = {
+    cb_fuelvalve,
+    cb_fuelqty,
+    cb_fuelpressure,
+    cb_genreset,
+    cb_genfield,
+    cb_xmsntemp,
+    cb_xmsnpress,
+    cb_engmgt
+  },
+  [6] = {
+    cb_engtrq,
+    cb_engng,
+    cb_engnp,
+    cb_engnr,
+    cb_engoiltemp,
+    cb_engoilpress,
+    cb_engantiice,
+    cb_starter
+  },
+  [7] = {
+    cb_igniter,
+    cb_fadec,
+    cb_hydsys,
+    cb_pedalstop,
+    cb_ldglights,
+    cb_ldglightscont,
+    cb_cautionlights,
+    cb_oatv
+  },
+  [8] = {
+    cb_amps,
+    cb_navcom1,
+    cb_com2,
+    cb_xpndr,
+    cb_gps1,
+    cb_gps2,
+    cb_radalt,
+    cb_4h8unused
   }
 }
