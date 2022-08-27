0c1ee52d04996000a1173f7364c90bd480467903
mastersta <37850367+mastersta@users.noreply.github.com>
2022-08-27 14:45:58 -0400

    Add 206L3 landing lights and floats commands to flywithlua script

--- flywithlua/407_landing_lights.lua
+++ flywithlua/407_landing_lights.lua
@@ -23,3 +23,41 @@ if (PLANE_ICAO == "B407") then
                 "XPLMSetDataf(g407floatDataRef, 0)",
                 "")
 end
+
+if (PLANE_ICAO == "206L3") then
+	g206landinglightDataRef = XPLMFindDataRef("206L3/ldglts/sw")
+
+	create_command( "FlyWithLua/206/landing_lights_off", "Landing Lights Off",
+                "",
+                "XPLMSetDatai(g206landinglightDataRef, 0)",
+                "")
+
+	create_command( "FlyWithLua/206/landing_lights_fwd", "Landing Lights Forward",
+                "",
+                "XPLMSetDatai(g206landinglightDataRef, 1)",
+                "")
+
+	create_command( "FlyWithLua/206/landing_lights_both", "Landing Lights Both",
+                "",
+                "XPLMSetDatai(g206landinglightDataRef, 2)",
+                "")
+
+	g206floatArmDataRef = XPLMFindDataRef("206L3/acc/floats_active")
+
+	create_command( "FlyWithLua/206/float_disarm", "Float Disarm",
+                "",
+                "XPLMSetDatai(g206floatArmDataRef, 0)",
+                "")
+
+	create_command( "FlyWithLua/206/float_arm", "Float Arm",
+                "",
+                "XPLMSetDatai(g206floatArmDataRef, 1)",
+                "")
+
+	g206floatBlowDataRef = XPLMFindDataRef("206L3/acc/floats_blown")
+
+	create_command( "FlyWithLua/206/float_blow", "Float Blow",
+                "",
+                "XPLMSetDatai(g206floatBlowDataRef, 0)",
+                "")
+end
